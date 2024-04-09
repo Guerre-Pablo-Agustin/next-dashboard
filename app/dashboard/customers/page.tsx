5e8d746ead58efe9de1e4e373288a6adf30bc471
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2024-04-09 20:15:28 -0300

    add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,19 @@
-export default function Customers() {
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function CustomerPage({ searchParams }: { searchParams?: { query?: string; page?: string } }) {
+    const query = searchParams?.query || '';
+  
+    const customers = await fetchFilteredCustomers(query);
+  
     return (
-        <div>
-            <h1>Customers</h1>
+        <div className="w-full">
+            <Suspense fallback={<InvoicesTableSkeleton />}>
+           <CustomersTable  customers={customers}/>
+            </Suspense>
         </div>
     );
 }
