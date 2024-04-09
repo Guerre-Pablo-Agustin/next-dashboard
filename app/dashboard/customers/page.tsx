0144f5ac7851d986aa6a07c71f49b19ab69b3560
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2024-04-09 20:54:37 -0300

    add

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,5 @@
 import { fetchFilteredCustomers } from "@/app/lib/data";
-import { FormattedCustomersTable } from "@/app/lib/definitions";
 import CustomersTable from "@/app/ui/customers/table";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
 
 export default async function CustomerPage({ searchParams }: { searchParams?: { query?: string; page?: string } }) {
     const query = searchParams?.query || '';
@@ -11,9 +8,7 @@ export default async function CustomerPage({ searchParams }: { searchParams?: { 
   
     return (
         <div className="w-full">
-            <Suspense fallback={<InvoicesTableSkeleton />}>
            <CustomersTable  customers={customers}/>
-            </Suspense>
         </div>
     );
 }
